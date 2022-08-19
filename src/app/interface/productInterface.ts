e03c363b523f004590d440ffb3bf35715aaa9c50
Fablena <fablena_dias@hotmail.com>
2022-08-19 17:18:19 -0300

    feat: implementing the product's 'interface' and 'schema'.

---
+++ src/app/interface/productInterface.ts
@@ -0,0 +1,26 @@
+import { Types } from 'mongoose';
+
+
+export interface ProductInterface {
+    title: string;
+    description: string;
+    department: string;
+    brand: string;
+    price: number;
+    qtd_stock: number;
+    bar_codes: string;
+    
+}
+
+export interface ProductInterfaceResponse {
+    _id: Types.ObjectId;
+    title: string;
+    description: string;
+    department: string;
+    brand: string;
+    price: number
+    qtd_stock: number;
+    stock_control_enabled: boolean;
+    bar_codes: string;
+
+}
---
+++ src/app/schema/productSchema.ts
@@ -0,0 +1,21 @@
+import { Schema } from 'mongoose';
+import { ProductInterface } from '../interface/productInterface'
+
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const schema = new Schema<ProductInterface>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  department: { type: String, required: true },
+  brand: { type: String, required: true },
+  price: { type: Number, required: true },
+  qtd_stock: { type: Number, required: true },
+  bar_codes: { type: String, unique: true, required: true },
+
+});
+
+schema.plugin(mongoosePaginate);
+const Product = mongoose.model('product', schema);
+
+export default Product;
